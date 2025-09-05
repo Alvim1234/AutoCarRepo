b04a6304f7a2d4244e8b1ba6adeaaf60515691d4
Alvim1234 <lucasalvimetec@gmail.com>
2025-09-05 10:14:45 -0300

    Terminado

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -4,5 +4,8 @@ public class ClassePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	}
+		ClasseCarros obj1 = new ClasseCarros("Azul", "Wolksvagen", "HB20", "Elétrico", 4);
+		ClasseMotos obj2 = new ClasseMotos("Vermelho", "BMW", "BIZ", "Manual");
+		ClasseCaminhoes obj3 = new ClasseCaminhoes("Branco", "Scania", "Tróia", "Manual", 1000);
+	}	
 }
