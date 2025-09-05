5a19ca851e27507bcce460c631a1824d1341b256
Alvim1234 <lucasalvimetec@gmail.com>
2025-09-05 09:17:08 -0300

    Adicionada classe motos herdando atributos e métodos da classe

--- src/ClasseCarros.java
+++ src/ClasseCarros.java
@@ -1,7 +1,7 @@
 
 public class ClasseCarros extends ClasseAutomotores{
 
-	public ClasseCarros(String corPar, String marcaPar, String modeloPar, String tipoPar, float cargaSuportadaPar) {
+	public ClasseCarros(String corPar, String marcaPar, String modeloPar, String tipoPar) {
 
 		super(corPar, marcaPar, modeloPar, tipoPar);
 	}
