08f0f15a6482055d0e06b769ca312d793b944955
Alvim1234 <lucasalvimetec@gmail.com>
2025-09-05 09:35:03 -0300

    Adicionados getters e setters das classes filhas.

--- src/ClasseCarros.java
+++ src/ClasseCarros.java
@@ -1,8 +1,18 @@
 
 public class ClasseCarros extends ClasseAutomotores{
 
-	public ClasseCarros(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+	int quantportasAt;
+	
+	public ClasseCarros(String corPar, String marcaPar, String modeloPar, String tipoPar, int quantportasPar) {
 
 		super(corPar, marcaPar, modeloPar, tipoPar);
+		this.quantportasAt = quantportasPar;
+	}
+	public int pegarQuantportas() {
+	    return quantportasAt;
+	}
+	
+	public void alterarQuantport(int quantportasPar) {
+	    this.quantportasAt = quantportasPar;
 	}
 }
